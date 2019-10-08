65995967188ca77a37cd28423e136fff275d97d6
dai00035 <dai00035@algonquinlive.com>
2019-10-08 13:26:06 -0400

    Not on the lines

--- ex2.py
+++ ex2.py
@@ -4,7 +4,7 @@ def main():
 def is_point_in_box (x, y, xmin, ymin, xmax, ymax):
     """Given a point and an area(x,y, xmin, ymin, xmax, ymax), to see if it is in the provided area
     """
-    if x >= xmin and x <= xmax and y >= ymin and y <= ymax :
+    if x > xmin and x < xmax and y > ymin and y < ymax :
         return True
     else :
         return False
